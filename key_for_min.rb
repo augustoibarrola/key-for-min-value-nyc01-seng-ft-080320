d4465155dc5b837db84bc91996611c58306ce57b
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-23 19:12:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   lo_v = 0
   lo_k = nil
   name_hash.each do |k, v|
-    if lo_v = 0 or v < lo_v
+    if lo_v == 0 or v < lo_v
       lo_v = v
       lo_k = k
     end
