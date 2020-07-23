3356161f778f8d6ea94c01ea9804cb5d0b83a6b1
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-23 19:12:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lo_v = 0 
+  lo_k = nil 
+  name_hash.each do |k, v|
+    if lo_v = 0 or v < lo_v
+      lo_v = v
+      lo_k = k
+    end
+  end
+  lo_k
 end
 
 =beging
